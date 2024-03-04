cefdccc89f7747266dc07f96463c3701a64e1d8d
Jacob Calvillo <jakeob99@gmail.com>
2024-03-04 17:38:58 -0600

    adding styles to user registration page

--- src/pages/Users/UserPage.jsx
+++ src/pages/Users/UserPage.jsx
@@ -1,13 +1,15 @@
-import { FormControl, Container, Grid, Typography, Button } from '@mui/material';
+import { FormControl, Container, Grid, Typography, Button, TextField } from '@mui/material';
 import CustomLabelForm from '../../components/UserComponents/CustomLabelForm';
 import * as React from 'react';
 import dayjs from 'dayjs';  
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Alert from '@mui/material/Alert';
 
 export default function UserPage() {
   const [value, setValue] = React.useState(dayjs());
+  const [cleared, setCleared] = React.useState(false);
 
   const handleDateChange = (newValue) => {
     setValue(newValue);
@@ -16,6 +18,16 @@ export default function UserPage() {
   const handleSubmit = () => {
     // Aquí puedes implementar la lógica para manejar la creación de usuarios
   };
+
+  React.useEffect(() => {
+    let timeout;
+    if (cleared) {
+      timeout = setTimeout(() => {
+        setCleared(false);
+      }, 1000); 
+    }
+    return () => clearTimeout(timeout);
+  }, [cleared]);
 
   return (
     <Container style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
@@ -40,18 +52,27 @@ export default function UserPage() {
                 label="Fecha de nacimiento"
                 value={value}
                 onChange={handleDateChange}
-                inputFormat="DD/MM/YYYY"
+                slotProps={{
+                  field: { clearable: true, onClear: () => setCleared(true) },
+                }}
                 renderInput={(params) => (
-                  <input
-                    {...params.inputProps}
+                  <TextField
+                    {...params}
                     type="text"
                     placeholder="Seleccione una fecha"
-                    readOnly
-                    style={{ width: '100%' }}
+                    fullWidth
                   />
                 )}
               />
             </LocalizationProvider>
+            {cleared && (
+              <Alert
+                sx={{ position: 'absolute', bottom: 0, right: 0 }}
+                severity="success"
+              >
+                ¡Campo borrado!
+              </Alert>
+            )}
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
