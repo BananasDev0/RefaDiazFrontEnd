ceb1487f8e839c4fa747e50139d635b93c106f63
Roel Mendoza <roelmdz@gmail.com>
2024-03-09 09:07:46 -0600

    Added brandviewer

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,18 +1,37 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
 
 export const ItemCard = ({ item, columns }) => {
+    console.log(item.imageUrl)
     return (
-      <Card sx={{ margin: 2 }}>
-        <CardContent>
+      <Card sx={{ margin: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {item.imageUrl && (
+          <CardMedia
+            component="img"
+            sx={{ width: 160, height: 160, objectFit: 'contain'  }} // Ajusta estas dimensiones según necesites
+            image={item.imageUrl}
+            alt={`Imagen de ${item.name}`}
+          />
+        )}
+        <CardContent sx={{ width: '100%' }}>
           {columns.map((column) => (
-            <Box key={column.field} sx={{ display: 'flex', alignItems: 'center', marginBottom: 1 }}>
-              <Typography variant="body2" color="textSecondary" sx={{ marginRight: 1, fontWeight: 'bold' }}>
-                {column.headerName}:
-              </Typography>
-              <Typography variant="body2">
+            <Box key={column.field} sx={{ display: 'flex', justifyContent: 'center', marginBottom: 1 }}>
+              {column.showLabel !== false && (
+                <Typography 
+                  sx={{ 
+                    marginRight: 1, 
+                    fontWeight: 'bold', 
+                    fontSize: '12px'
+                  }}
+                  color="textSecondary"
+                >
+                  {column.headerName}:
+                </Typography>
+              )}
+              <Typography sx={{ fontSize: '12px', ...(column.valueStyle || {}) }}>
                 {item[column.field]}
               </Typography>
             </Box>
@@ -20,4 +39,4 @@ export const ItemCard = ({ item, columns }) => {
         </CardContent>
       </Card>
     );
-  };
+};
