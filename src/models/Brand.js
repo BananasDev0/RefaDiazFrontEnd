02bd711a841a0d39187525c2a2491b9caffa6a75
Jacob Calvillo <jakeob99@gmail.com>
2024-03-26 14:47:35 -0600

    adding brandType field in brand model

--- src/models/Brand.js
+++ src/models/Brand.js
@@ -1,7 +1,8 @@
 export default class Brand {
-    constructor({id, name, imageUrl}) {
+    constructor({id, name, imageUrl, brandType}) {
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
+        this.brandType = brandType;
     }
 }
